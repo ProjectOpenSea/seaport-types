86ab2151147e402ebf47531e9a49699868597591
James Wenzel <jameswenzel@users.noreply.github.com>
2023-05-09 17:27:55 -0700

    update zoneinterface

--- src/interfaces/ZoneInterface.sol
+++ src/interfaces/ZoneInterface.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import { ZoneParameters, Schema } from "../lib/ConsiderationStructs.sol";
+import {ZoneParameters, Schema} from "../lib/ConsiderationStructs.sol";
+
+import {IERC165} from "../interfaces/IERC165.sol";
 
 /**
  * @title  ZoneInterface
  * @notice Contains functions exposed by a zone.
  */
-interface ZoneInterface {
+interface ZoneInterface is IERC165 {
     /**
      * @dev Validates an order.
      *
@@ -17,9 +19,7 @@ interface ZoneInterface {
      * @return validOrderMagicValue The magic value that indicates a valid
      *                              order.
      */
-    function validateOrder(
-        ZoneParameters calldata zoneParameters
-    ) external returns (bytes4 validOrderMagicValue);
+    function validateOrder(ZoneParameters calldata zoneParameters) external returns (bytes4 validOrderMagicValue);
 
     /**
      * @dev Returns the metadata for this zone.
@@ -27,11 +27,7 @@ interface ZoneInterface {
      * @return name The name of the zone.
      * @return schemas The schemas that the zone implements.
      */
-    function getSeaportMetadata()
-        external
-        view
-        returns (
-            string memory name,
-            Schema[] memory schemas // map to Seaport Improvement Proposal IDs
-        );
+    function getSeaportMetadata() external view returns (string memory name, Schema[] memory schemas); // map to Seaport Improvement Proposal IDs
+
+    function supportsInterface(bytes4 interfaceId) external view override returns (bool);
 }
