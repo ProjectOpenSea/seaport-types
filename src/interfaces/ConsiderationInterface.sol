ce8870ec14b5e6e6f8f53a260fe006e39a2d407f
moshui <sme_moshui@outlook.com>
2023-08-24 11:02:32 +0800

    remove useless interface

--- src/interfaces/ConsiderationInterface.sol
+++ src/interfaces/ConsiderationInterface.sol
@@ -72,53 +72,6 @@ interface ConsiderationInterface {
     ) external payable returns (bool fulfilled);
 
     /**
-     * @notice Fill an order, fully or partially, with an arbitrary number of
-     *         items for offer and consideration alongside criteria resolvers
-     *         containing specific token identifiers and associated proofs.
-     *
-     * @param advancedOrder       The order to fulfill along with the fraction
-     *                            of the order to attempt to fill. Note that
-     *                            both the offerer and the fulfiller must first
-     *                            approve this contract (or their preferred
-     *                            conduit if indicated by the order) to transfer
-     *                            any relevant tokens on their behalf and that
-     *                            contracts must implement `onERC1155Received`
-     *                            to receive ERC1155 tokens as consideration.
-     *                            Also note that all offer and consideration
-     *                            components must have no remainder after
-     *                            multiplication of the respective amount with
-     *                            the supplied fraction for the partial fill to
-     *                            be considered valid.
-     * @param criteriaResolvers   An array where each element contains a
-     *                            reference to a specific offer or
-     *                            consideration, a token identifier, and a proof
-     *                            that the supplied token identifier is
-     *                            contained in the merkle root held by the item
-     *                            in question's criteria element. Note that an
-     *                            empty criteria indicates that any
-     *                            (transferable) token identifier on the token
-     *                            in question is valid and that no associated
-     *                            proof needs to be supplied.
-     * @param fulfillerConduitKey A bytes32 value indicating what conduit, if
-     *                            any, to source the fulfiller's token approvals
-     *                            from. The zero hash signifies that no conduit
-     *                            should be used, with direct approvals set on
-     *                            Consideration.
-     * @param recipient           The intended recipient for all received items,
-     *                            with `address(0)` indicating that the caller
-     *                            should receive the items.
-     *
-     * @return fulfilled A boolean indicating whether the order has been
-     *                   successfully fulfilled.
-     */
-    function fulfillAdvancedOrder(
-        AdvancedOrder calldata advancedOrder,
-        CriteriaResolver[] calldata criteriaResolvers,
-        bytes32 fulfillerConduitKey,
-        address recipient
-    ) external payable returns (bool fulfilled);
-
-    /**
      * @notice Match an arbitrary number of orders, each with an arbitrary
      *         number of items for offer and consideration along with a set of
      *         fulfillments allocating offer components to consideration
